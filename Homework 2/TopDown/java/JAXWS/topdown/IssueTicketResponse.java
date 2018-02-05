00e00a91a3edc86f415b7a6b3e84485df1023304
bharathkarthikeyan <bharath.karthikeyan@gmail.com>
2018-02-05 22:32:01 +0100

    Initial Commit - HW2

---
+++ Homework 2/TopDown/java/JAXWS/topdown/IssueTicketResponse.java
@@ -0,0 +1,62 @@
+
+package JAXWS.topdown;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for issueTicketResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="issueTicketResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://topdown.se/}ticket"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "issueTicketResponse", propOrder = {
+    "_return"
+})
+public class IssueTicketResponse {
+
+    @XmlElement(name = "return", required = true)
+    protected Ticket _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Ticket }
+     *     
+     */
+    public Ticket getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Ticket }
+     *     
+     */
+    public void setReturn(Ticket value) {
+        this._return = value;
+    }
+
+}
---
+++ Homework 2/TopDown/java/JAXWS/topdown/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://topdown.se/")
+package JAXWS.topdown;
